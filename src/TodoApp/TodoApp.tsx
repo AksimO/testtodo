8d46a9824d46c560de4b72b47552099cb939dfb5
Maksim <m.romanenko@snaswap.eu>
2020-06-19 22:06:01 +0200

    Empty todo list

--- src/TodoApp/TodoApp.tsx
+++ src/TodoApp/TodoApp.tsx
@@ -1,4 +1,10 @@
 import * as React from "react";
 import "./TodoApp.css";
+import { TodoList } from "../TodoList/TodoList";
 
-export const TodoApp = () => <div className="TodoApp">TodoApp</div>;
+export const TodoApp = () => (
+  <div className="TodoApp">
+    <h1>TodoApp</h1>
+    <TodoList />
+  </div>
+);
